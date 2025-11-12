6eb05b7473914a2fdf0f5b858e496c09c95f8797
Bruno Germano <bruno.germano@gmail.com>
2025-11-12 10:58:22 -0300

    feat: implements ai feature

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,6 +1,44 @@
-import { Hono } from 'hono'
-const app = new Hono()
+import { createClient } from "azion/ai";
+import { Hono } from "hono";
 
-app.get('/', (c) => c.text('Hello Hono Azion!'))
+const app = new Hono();
 
-app.fire()
+app.get("/", (c) => c.text("Hello Hono Azion!"));
+
+const mainPrompt = `Gere um dilema "Would you rather" em PT-BR. Responda JSON com optionA, optionB, theme, difficulty. Evite conteúdo sensível. 
+   Se respeitar o tema informado.
+   Se respeitar a dificuldade informada.
+   Responda apenas o JSON, sem nenhuma notação extra ou texto que não seja a resposta em JSON.
+  `;
+
+const themes = [
+  "Plataforma da Azion",
+  "Desenvolvimento",
+  "Marketing",
+  "Design",
+  "Marketing",
+];
+
+app.get("/chat", async (c) => {
+  const theme = c.req.query("theme") || "Plataforma da Azion";
+  const difficulty = c.req.query("difficulty") || "easy";
+
+  const client = createClient({
+    token: process.env.AZION_TOKEN,
+  });
+
+  const response = await client.chat({
+    messages: [
+      {
+        role: "user",
+        content: mainPrompt,
+      },
+      { role: "user", content: `Tema: ${theme}. Dificuldade: ${difficulty}.` },
+    ],
+    stream: false,
+  });
+
+  return c.json(response.data);
+});
+
+app.fire();
